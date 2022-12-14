9209fb10a46f7b4db63f27882a2a35703ea7e496
ChrisKLoveless <chriskloveless@gmail.com>
2022-12-14 10:05:10 -0800

    add passing test for Tama constructor paramaters
    co-authored-by: Teddy Atkinson <tedddy.atkinson@gmail.com>

--- Tamagotchi.Tests/ModelTests/TamaTests.cs
+++ Tamagotchi.Tests/ModelTests/TamaTests.cs
@@ -21,5 +21,14 @@ namespace Tamagotchi.Tests
       Tama tama = new Tama("cow", 1, 1, 1);
       Assert.AreEqual("cow", tama.Name);
     }
+
+    [TestMethod]
+    public void Constructor_SetName_String()
+    {
+      Tama tama = new Tama("cow", 1, 1, 1);
+      string newName = "moose";
+      tama.Name = newName;
+      Assert.AreEqual("moose", tama.Name);
+    }
   }
 }
